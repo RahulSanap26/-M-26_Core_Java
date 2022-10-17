0e211ab6aac19f51889be9c839baec4b6379f9b7
Dell <Dell@192.168.0.8>
2022-10-17 17:43:38 +0530

    Commit

---
+++ Batch M-26/src/org/tnsindia/trial/Demo.java
@@ -0,0 +1,11 @@
+package org.tnsindia.trial;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Rahul Rajesh Sanap");
+		// TODO Auto-generated method stub
+
+	}
+
+}
